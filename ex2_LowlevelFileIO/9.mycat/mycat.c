52acb7b7ab31fa426888053c3787abf106050054
RuDas-KR <jjm411@naver.com>
2015-04-28 22:26:56 +0900

    Add

---
+++ ex2_LowlevelFileIO/9.mycat/mycat.c
@@ -0,0 +1,30 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(char argc, char *argv[]) {
+	char ch;
+	int fd;
+
+	if(argc != 2) {
+		perror("Wrong Used");
+		printf("Used By : mycat Filename\n");
+		exit(1);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	printf("%d", fd);
+
+	if(fd == -1) {
+		perror(argv[1]);
+		exit(1);
+	}
+
+	while(read(fd, &ch, 1))
+		write(1, &ch, 1);
+
+	close(fd);
+
+	return 0;
+}
